84b595a09efd655cc97c91b5ce3e0818c2ef03c8
zainablawal2211 <zainablawal2211@gmail.com>
2024-10-22 22:12:20 +0100

    Fix: The bug causing crash

--- app/src/main/java/com/example/android/politicalpreparedness/representative/model/Representative.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/model/Representative.kt
@@ -4,7 +4,7 @@ package com.example.android.politicalpreparedness.representative.model
 import com.example.android.politicalpreparedness.network.models.Office
 import com.example.android.politicalpreparedness.network.models.Official
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Representative (
